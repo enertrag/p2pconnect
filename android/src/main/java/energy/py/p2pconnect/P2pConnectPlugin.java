c1cbee16c54b031fbbe9cb3fc3fdd9a7eb3e326a
Sven Sönnichsen <sven@sven-s.de>
2021-08-30 11:20:55 +0200

    add empty implementation

--- android/src/main/java/energy/py/p2pconnect/P2pConnectPlugin.java
+++ android/src/main/java/energy/py/p2pconnect/P2pConnectPlugin.java
@@ -12,11 +12,72 @@ public class P2pConnectPlugin extends Plugin {
     private P2pConnect implementation = new P2pConnect();
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void isAvailable(PluginCall call) {
+        JSObject ret = new JSObject();
+        ret.put("available", implementation.isAvailable());
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void startAdvertise(PluginCall call) {
 
         JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void stopAdvertise(PluginCall call) {
+
+        JSObject ret = new JSObject();
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void startBrowse(PluginCall call) {
+
+        JSObject ret = new JSObject();
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void stopBrowse(PluginCall call) {
+
+        JSObject ret = new JSObject();
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void connect(PluginCall call) {
+
+        JSObject ret = new JSObject();
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void disconnect(PluginCall call) {
+
+        JSObject ret = new JSObject();
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void send(PluginCall call) {
+
+        JSObject ret = new JSObject();
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void sendResource(PluginCall call) {
+
+        JSObject ret = new JSObject();
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void getProgress(PluginCall call) {
+
+        JSObject ret = new JSObject();
         call.resolve(ret);
     }
 }
